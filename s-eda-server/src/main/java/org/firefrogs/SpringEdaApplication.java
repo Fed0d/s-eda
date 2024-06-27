aeb65229907ba1b6099bf919bd1ae7145af4ee87
Fed0d <loldffgf@yandex.ru>
2024-06-27 20:51:42 +0300

    Сервер

---
+++ s-eda-server/src/main/java/org/firefrogs/SpringEdaApplication.java
@@ -0,0 +1,11 @@
+package org.firefrogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEdaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEdaApplication.class, args);
+    }
+}
